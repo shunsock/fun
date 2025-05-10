b360e6ad42ff284d988253c6a97467e9e7e5f1aa
shunsock <s.tsuchiya.business@gmail.com>
2025-05-10 21:44:49 +0900

    feature: add new function div

--- Fun/Program.fs
+++ Fun/Program.fs
@@ -4,6 +4,10 @@ type Number =
     | Integer of int
     | Float of float
 
+type RError = { Name: string; Message: string }
+
+type CalculationError = | ZeroDivisionError of RError
+
 let toInteger (number: Number) : Number =
     match number with
     | Integer i -> Integer (i)
@@ -38,3 +42,17 @@ let mul (x: Number) (y: Number) : Number =
     | (Float x, Integer y) -> fromFloat (x * float y)
     | (Integer x, Float y) -> fromFloat (float x * y)
     | (Float x, Float y) -> fromFloat (x * y)
+
+let div (x: Number) (y: Number) : Result<Number, CalculationError> =
+    match (x, y) with
+    | (_, Integer 0)
+    | (_, Float 0.0) ->
+        Error (
+            ZeroDivisionError
+                { Name = "ZeroDivisionError"
+                  Message = "Can not divide by zero" }
+        )
+    | (Integer x, Integer y) -> Ok (fromInteger (x / y))
+    | (Float x, Integer y) -> Ok (fromFloat (x / float y))
+    | (Integer x, Float y) -> Ok (fromFloat (float x / y))
+    | (Float x, Float y) -> Ok (fromFloat (x / y))
